e9521502de62614c554b04ca4cbcf81494f4b0fb
Anton Henriksson <henriksson.anton@gmail.com>
2026-04-16 21:19:42 +0200

    added some service methods

--- src/main/java/se/productservicespring/app/repo/ProductRepository.java
+++ src/main/java/se/productservicespring/app/repo/ProductRepository.java
@@ -3,5 +3,8 @@ package se.productservicespring.app.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.productservicespring.app.model.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByProductId(Long productId);
 }
--- src/main/java/se/productservicespring/app/service/ProductService.java
+++ src/main/java/se/productservicespring/app/service/ProductService.java
@@ -1,7 +1,15 @@
 package se.productservicespring.app.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import se.productservicespring.app.dto.ProductRequest;
+import se.productservicespring.app.dto.ProductResponse;
+import se.productservicespring.app.mapper.ProductMapper;
+import se.productservicespring.app.model.Product;
 import se.productservicespring.app.repo.ProductRepository;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -11,4 +19,40 @@ public class ProductService {
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
+    @Transactional
+    public List<ProductResponse> syncProducts(List<ProductRequest> requests) {
+        List<Product> productsToSave = requests
+                .stream()
+                .map(ProductMapper::toProduct)
+                .toList();
+        
+        List<Product> savedProducts = productRepository.saveAll(productsToSave);
+
+        return savedProducts
+                .stream()
+                .map(ProductMapper::toResponse)
+                .toList();
+    }
+
+    @Transactional
+    public List<ProductResponse> getProducts() {
+        if (productRepository.findAll().isEmpty()) {
+            throw new EntityNotFoundException("No products found");
+        }
+        return productRepository
+                .findAll()
+                .stream()
+                .map(ProductMapper::toResponse).toList();
+    }
+
+    @Transactional
+    public ProductResponse getProduct(Long productId) {
+        Product product = productRepository.findByProductId(productId)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found"));
+
+        return ProductMapper.toResponse(product);
+    }
+
 }
+
